1c1ef78cde6b920a728dcd87409bbb058bb3b014
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-03-27 23:22:51 +0530

    Api Gateway service added

---
+++ Api-Gateway/src/test/java/com/soumyajit/Api/Gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumyajit.Api.Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
