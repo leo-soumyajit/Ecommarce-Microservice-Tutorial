469fc6ae220d55f1eda1937b81e3772fae4609ac
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2025-04-05 02:16:59 +0530

    Centralized Configuration

---
+++ config-server/src/test/java/com/soumyajit/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soumyajit.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
